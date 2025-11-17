27dd8a8d9d3e186e9eda25101aa72b0035fd09b2
Si-hyperbole <96701481+Si-hyperbole@users.noreply.github.com>
2025-11-17 09:41:27 -0400

    Add project files.

---
+++ TicTacToeNov2025/MainForms.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace TicTacToeNov2025
+{
+    partial class LablePl2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AA = new System.Windows.Forms.Button();
+            this.AB = new System.Windows.Forms.Button();
+            this.AC = new System.Windows.Forms.Button();
+            this.BA = new System.Windows.Forms.Button();
+            this.BB = new System.Windows.Forms.Button();
+            this.BC = new System.Windows.Forms.Button();
+            this.CA = new System.Windows.Forms.Button();
+            this.CB = new System.Windows.Forms.Button();
+            this.CC = new System.Windows.Forms.Button();
+            this.BNewGame = new System.Windows.Forms.Button();
+            this.BExit = new System.Windows.Forms.Button();
+            this.ScooreCounterPl1 = new System.Windows.Forms.Label();
+            this.ScooreCounterPl2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Winner = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Player1Name = new System.Windows.Forms.TextBox();
+            this.Player2Name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AA
+            // 
+            this.AA.BackColor = System.Drawing.Color.White;
+            this.AA.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AA.Location = new System.Drawing.Point(195, 105);
+            this.AA.Name = "AA";
+            this.AA.Size = new System.Drawing.Size(100, 100);
+            this.AA.TabIndex = 0;
+            this.AA.UseVisualStyleBackColor = false;
+            this.AA.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // AB
+            // 
+            this.AB.BackColor = System.Drawing.Color.White;
+            this.AB.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AB.Location = new System.Drawing.Point(301, 105);
+            this.AB.Name = "AB";
+            this.AB.Size = new System.Drawing.Size(100, 100);
+            this.AB.TabIndex = 1;
+            this.AB.UseVisualStyleBackColor = false;
+            this.AB.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // AC
+            // 
+            this.AC.BackColor = System.Drawing.Color.White;
+            this.AC.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AC.Location = new System.Drawing.Point(407, 105);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(100, 100);
+            this.AC.TabIndex = 2;
+            this.AC.UseVisualStyleBackColor = false;
+            this.AC.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // BA
+            // 
+            this.BA.BackColor = System.Drawing.Color.White;
+            this.BA.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BA.Location = new System.Drawing.Point(195, 211);
+            this.BA.Name = "BA";
+            this.BA.Size = new System.Drawing.Size(100, 100);
+            this.BA.TabIndex = 3;
+            this.BA.UseVisualStyleBackColor = false;
+            this.BA.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // BB
+            // 
+            this.BB.BackColor = System.Drawing.Color.White;
+            this.BB.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BB.Location = new System.Drawing.Point(301, 211);
+            this.BB.Name = "BB";
+            this.BB.Size = new System.Drawing.Size(100, 100);
+            this.BB.TabIndex = 4;
+            this.BB.UseVisualStyleBackColor = false;
+            this.BB.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // BC
+            // 
+            this.BC.BackColor = System.Drawing.Color.White;
+            this.BC.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BC.Location = new System.Drawing.Point(407, 211);
+            this.BC.Name = "BC";
+            this.BC.Size = new System.Drawing.Size(100, 100);
+            this.BC.TabIndex = 5;
+            this.BC.UseVisualStyleBackColor = false;
+            this.BC.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // CA
+            // 
+            this.CA.BackColor = System.Drawing.Color.White;
+            this.CA.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CA.Location = new System.Drawing.Point(195, 317);
+            this.CA.Name = "CA";
+            this.CA.Size = new System.Drawing.Size(100, 100);
+            this.CA.TabIndex = 6;
+            this.CA.UseVisualStyleBackColor = false;
+            this.CA.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // CB
+            // 
+            this.CB.BackColor = System.Drawing.Color.White;
+            this.CB.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB.Location = new System.Drawing.Point(301, 317);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(100, 100);
+            this.CB.TabIndex = 7;
+            this.CB.UseVisualStyleBackColor = false;
+            this.CB.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // CC
+            // 
+            this.CC.BackColor = System.Drawing.Color.White;
+            this.CC.Font = new System.Drawing.Font("MV Boli", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CC.Location = new System.Drawing.Point(407, 317);
+            this.CC.Name = "CC";
+            this.CC.Size = new System.Drawing.Size(100, 100);
+            this.CC.TabIndex = 8;
+            this.CC.UseVisualStyleBackColor = false;
+            this.CC.Click += new System.EventHandler(this.BClickPlayerMoves);
+            // 
+            // BNewGame
+            // 
+            this.BNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNewGame.Location = new System.Drawing.Point(48, 340);
+            this.BNewGame.Name = "BNewGame";
+            this.BNewGame.Size = new System.Drawing.Size(97, 45);
+            this.BNewGame.TabIndex = 12;
+            this.BNewGame.Text = "New Game";
+            this.BNewGame.UseVisualStyleBackColor = true;
+            this.BNewGame.Click += new System.EventHandler(this.BNewGame_Click);
+            // 
+            // BExit
+            // 
+            this.BExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BExit.Location = new System.Drawing.Point(685, 340);
+            this.BExit.Name = "BExit";
+            this.BExit.Size = new System.Drawing.Size(100, 45);
+            this.BExit.TabIndex = 13;
+            this.BExit.Text = "Exit";
+            this.BExit.UseVisualStyleBackColor = true;
+            this.BExit.Click += new System.EventHandler(this.BExit_Click);
+            // 
+            // ScooreCounterPl1
+            // 
+            this.ScooreCounterPl1.AutoSize = true;
+            this.ScooreCounterPl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScooreCounterPl1.Location = new System.Drawing.Point(63, 149);
+            this.ScooreCounterPl1.Name = "ScooreCounterPl1";
+            this.ScooreCounterPl1.Size = new System.Drawing.Size(32, 36);
+            this.ScooreCounterPl1.TabIndex = 14;
+            this.ScooreCounterPl1.Text = "0";
+            // 
+            // ScooreCounterPl2
+            // 
+            this.ScooreCounterPl2.AutoSize = true;
+            this.ScooreCounterPl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScooreCounterPl2.Location = new System.Drawing.Point(696, 149);
+            this.ScooreCounterPl2.Name = "ScooreCounterPl2";
+            this.ScooreCounterPl2.Size = new System.Drawing.Size(32, 36);
+            this.ScooreCounterPl2.TabIndex = 15;
+            this.ScooreCounterPl2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "Winer";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(325, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 20;
+            // 
+            // Winner
+            // 
+            this.Winner.AccessibleName = "Who Wins";
+            this.Winner.AutoSize = true;
+            this.Winner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Winner.Location = new System.Drawing.Point(227, 12);
+            this.Winner.Name = "Winner";
+            this.Winner.Size = new System.Drawing.Size(0, 29);
+            this.Winner.TabIndex = 21;
+            this.Winner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 29);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(651, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 29);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Player 2";
+            // 
+            // Player1Name
+            // 
+            this.Player1Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Name.Location = new System.Drawing.Point(12, 55);
+            this.Player1Name.Name = "Player1Name";
+            this.Player1Name.Size = new System.Drawing.Size(172, 36);
+            this.Player1Name.TabIndex = 24;
+            this.Player1Name.Text = "Player 1";
+            // 
+            // Player2Name
+            // 
+            this.Player2Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Name.Location = new System.Drawing.Point(572, 55);
+            this.Player2Name.Name = "Player2Name";
+            this.Player2Name.Size = new System.Drawing.Size(185, 36);
+            this.Player2Name.TabIndex = 25;
+            this.Player2Name.Text = "Player 2";
+            this.Player2Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // LablePl2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Player2Name);
+            this.Controls.Add(this.Player1Name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Winner);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ScooreCounterPl2);
+            this.Controls.Add(this.ScooreCounterPl1);
+            this.Controls.Add(this.BExit);
+            this.Controls.Add(this.BNewGame);
+            this.Controls.Add(this.CC);
+            this.Controls.Add(this.CB);
+            this.Controls.Add(this.CA);
+            this.Controls.Add(this.BC);
+            this.Controls.Add(this.BB);
+            this.Controls.Add(this.BA);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.AB);
+            this.Controls.Add(this.AA);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "LablePl2";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.LablePl2_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AA;
+        private System.Windows.Forms.Button AB;
+        private System.Windows.Forms.Button AC;
+        private System.Windows.Forms.Button BA;
+        private System.Windows.Forms.Button BB;
+        private System.Windows.Forms.Button BC;
+        private System.Windows.Forms.Button CA;
+        private System.Windows.Forms.Button CB;
+        private System.Windows.Forms.Button CC;
+        private System.Windows.Forms.Button BNewGame;
+        private System.Windows.Forms.Button BExit;
+        private System.Windows.Forms.Label ScooreCounterPl1;
+        private System.Windows.Forms.Label ScooreCounterPl2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Winner;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Player1Name;
+        private System.Windows.Forms.TextBox Player2Name;
+    }
+}
+
---
+++ TicTacToeNov2025/MainForms.cs
@@ -0,0 +1,248 @@
+﻿/// Max Csikos W# 0526040
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeNov2025
+{
+    public partial class LablePl2 : Form
+    {
+        public LablePl2()
+        {
+            InitializeComponent();
+        }
+
+        bool playerTurn = true;// true = 'X',false = 'O'
+        int[,] bordArray = new int[3, 3];
+        int[,] magicSquare = { { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } }; // rows, colum, diagnoals sum to 15. 3x3 square == 15
+        private int _tieCounter = 0;
+        private int _winingScoreX = 0;
+        private int _winningScoreO = 0;
+        bool bordEnable = true;
+        //bool winningSquar;
+        //bool hilightLight = true;
+
+        //private int _moveCounter = 0;
+        public void CheckWinner(int x, int y)
+        {
+            _tieCounter++;
+            int colums = 0;
+            int rows = 0;
+            int diagonal1 = 0;
+            int anitDiagonal1 = 0;
+
+            for (int i = 0; i < 3; i++)//counts the colums
+            {
+                colums += bordArray[i, y] * magicSquare[i, y];
+                rows += bordArray[x, i] * magicSquare[x, i];
+                //Console.WriteLine("Colums check for winner For Loop");
+            }
+            for (int i = 0; i < 3; i++)// counts the diagnoals
+            {
+                diagonal1 += bordArray[i, i] * magicSquare[i, i];
+                anitDiagonal1 += bordArray[i, 2 - i] * magicSquare[i, 2 - i];
+                //Console.WriteLine(" Diagonal check for winner For Loop");
+            }
+
+            if (colums == 15 || rows == 15 || diagonal1 == 15 || anitDiagonal1 == 15)
+            {
+                bordEnable = false;
+                Winner.Text = ($"The Winner Is {Player1Name.Text}");
+                _winingScoreX++;
+                ScooreCounterPl1.Text = _winingScoreX.ToString();
+                BNewGame.Enabled = true;
+                HightLightWinner(x, y, rows, colums, diagonal1, anitDiagonal1);
+            }
+            else if (colums == 30 || rows == 30 || anitDiagonal1 == 30 || diagonal1 == 30)/// "O" wins
+            {
+                Winner.Text = ($"The Winner Is {Player2Name.Text}");
+                _winningScoreO++;
+                ScooreCounterPl2.Text = _winningScoreO.ToString();
+                BNewGame.Enabled = true;
+                HightLightWinner(x, y, rows, colums, diagonal1, anitDiagonal1);
+
+                //ScooreCounterPl2.Text = winningScore.ToString();
+            }
+            else if (_tieCounter == 9)// tie counter
+            {
+                Winner.Text = _tieCounter.ToString("Its a Tie");
+                BNewGame.Enabled = true;
+            }
+        }// end of check winner method
+        private void HightLightWinner(int x, int y, int rows, int colums, int diagonal1, int anitDiagonal1)
+        {
+            Color winnningSquar = Color.Green;
+            if (rows == 15 || rows == 30)
+            {
+                switch (x)
+                {
+                    case 0:
+                        AA.BackColor = winnningSquar;
+                        BA.BackColor = winnningSquar;
+                        CA.BackColor = winnningSquar;
+                        break;
+                    case 1:
+                        AB.BackColor = winnningSquar;
+                        BB.BackColor = winnningSquar;
+                        CB.BackColor = winnningSquar;
+                        break;
+                    case 2:
+                        AC.BackColor = winnningSquar;
+                        BC.BackColor = winnningSquar;
+                        CC.BackColor = winnningSquar;
+                        break;
+                }
+            }
+            if (colums == 15 || colums == 30)
+            {
+                switch (y)
+                {
+                    case 0:
+                        AA.BackColor = winnningSquar;
+                        AB.BackColor = winnningSquar;
+                        AC.BackColor = winnningSquar;
+                        break;
+                    case 1:
+                        BA.BackColor = winnningSquar;
+                        BB.BackColor = winnningSquar;
+                        BC.BackColor = winnningSquar;
+                        break;
+                    case 2:
+                        CA.BackColor = winnningSquar;
+                        CB.BackColor = winnningSquar;
+                        CC.BackColor = winnningSquar;
+                        break;
+                }
+            }
+            if (diagonal1 == 15 || diagonal1 == 30)
+            {
+                AA.BackColor = winnningSquar;
+                BB.BackColor = winnningSquar;
+                CC.BackColor = winnningSquar;
+
+            }
+            else if (anitDiagonal1 == 15 || anitDiagonal1 == 30)
+            {
+                AC.BackColor = winnningSquar;
+                BB.BackColor = winnningSquar;
+                CA.BackColor = winnningSquar;
+            }
+        }
+        private void BClickPlayerMoves(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int colum = 0;
+            int row = 0;
+            //bord = true buttons enabled
+            if (bordEnable)
+            {
+                // represents X access in boardarray
+                switch (button.Name[0])
+                {
+                    case 'A':
+                        row = 0;
+                        //Console.WriteLine("Row Button 'A' has been pressed");
+                        break;
+                    case 'B':
+                        row = 1;
+                        //Console.WriteLine("Row buttion 'B' hass been pressed");
+                        break;
+                    case 'C':
+                        row = 2;
+                        //Console.WriteLine("Row buttion 'C' hass been pressed");
+                        break;
+                }// end of switch row
+
+                // represents y access in boardarray
+                switch (button.Name[1])
+                {
+                    case 'A':
+                        colum = 0;
+                        //Console.WriteLine("colum button 'A' has been pressed");
+                        break;
+                    case 'B':
+                        colum = 1;
+                        //Console.WriteLine("colum buttion 'B' hass been pressed");
+                        break;
+                    case 'C':
+                        colum = 2;
+                        //Console.WriteLine("colum buttion 'C' hass been pressed");
+                        break;
+                }// end of switch colum
+                // players moves
+                if (playerTurn)
+                {
+                    button.Text = "X";
+                    bordArray[colum, row] = 1;
+                    playerTurn = false;
+                    BNewGame.Enabled = false;
+                    //Console.WriteLine("Player X turn");
+                }
+                else
+                {
+                    button.Text = "O";
+                    bordArray[colum, row] = 2;
+                    playerTurn = true;
+                    //Console.WriteLine("Player O turn");
+                }
+                button.Enabled = false;
+                CheckWinner(colum, row);
+            }//end boardEnable
+        }// end of button click method
+        
+        private void BNewGame_Click(object sender, EventArgs e)
+        {
+            // reset all buttoonds and make them active
+            AA.Text = ""; AA.Enabled = true;
+            AB.Text = ""; AB.Enabled = true;
+            AC.Text = ""; AC.Enabled = true;
+            BA.Text = ""; BA.Enabled = true;
+            BB.Text = ""; BB.Enabled = true;
+            BC.Text = ""; BC.Enabled = true;
+            CA.Text = ""; CA.Enabled = true;
+            CB.Text = ""; CB.Enabled = true;
+            CC.Text = ""; CC.Enabled = true;
+            // Reset background color
+            Color winnningSquar = Color.White;
+            AA.BackColor = winnningSquar;
+            AB.BackColor = winnningSquar;
+            AC.BackColor = winnningSquar;
+            BA.BackColor = winnningSquar;
+            BB.BackColor = winnningSquar;
+            BC.BackColor = winnningSquar;
+            CA.BackColor = winnningSquar;
+            CB.BackColor = winnningSquar;
+            CC.BackColor = winnningSquar;
+            Winner.Text = "";
+            bordArray = new int[3, 3];
+            _tieCounter = 0;
+            playerTurn = true;
+            bordEnable = true;
+        }//end of BNewGame
+
+        private void BExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void LablePl2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LablePl2_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
